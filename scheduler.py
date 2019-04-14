ec25916fb32217ed0b4699688447d69a139f2657
Julian Rachman <jmrachman@gmail.com>
2019-04-14 00:18:59 -0700

    created json of all problems categorized by difficulty and created specific urls

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,9 @@
 #stucture: problems = {1: [question_title_slug, question_title_slug, ...], 2: [question_title_slug, question_title_slug, ...], 3: [question_title_slug, question_title_slug, ...]}
 
+#weekdays: 1 medium, 1 easy, 2 days work, 1 day break
+#weekends: which ever
 import requests
+#import webbrowser
 
 url_problem_prefix = "https://leetcode.com/problems/"
 
@@ -18,3 +21,6 @@ if __name__ == "__main__":
     print(len(data.get("stat_status_pairs", [])))
     print(dict_of_problems) #grabs the title slugs (used for suffix of urls)
     print(len(dict_of_problems[1]), len(dict_of_problems[2]), len(dict_of_problems[3]))
+
+    problem_url = f"{url_problem_prefix}{dict_of_problems[1][0]}"
+    #webbrowser.open(problem_url)
